81f7719ffe5d04735ed1c737ce87ee34dd11cd4a
Denis Blazhkun <dnzg666@gmail.com>
2018-03-06 18:21:14 +0300

    intro done

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,17 @@
 (function () {
-  return
+  $(document).ready(function () {
+    $('.intro__slider').slick({
+      centerMode: true,
+      dots: true,
+      autoplay: true,
+      draggable: false,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      autoplaySpeed: 4000,
+      centerPadding: '0px',
+      slidesToShow: 1,
+      nextArrow: '<button type="button" class="slider-button-next">Next</button>',
+      prevArrow: '<button type="button" class="slider-button-prev">Next</button>',
+    });
+  });
 })();
